f6a6fa1c13af960e821946bec9c9bfe3d3f99bd5
sdp8483 <17242713+sdp8483@users.noreply.github.com>
2019-09-21 08:22:32 -0400

    added full copy of marlin as backup

--- Marlin-bugfix-2.0.x/Marlin/Configuration.h
+++ Marlin-bugfix-2.0.x/Marlin/Configuration.h
@@ -905,8 +905,8 @@
  *     O-- FRONT --+
  *   (0,0)
  */
-#define X_PROBE_OFFSET_FROM_EXTRUDER 22.5    // X offset: -left  +right  [of the nozzle]
-#define Y_PROBE_OFFSET_FROM_EXTRUDER 4.5     // Y offset: -front +behind [the nozzle]
+#define X_PROBE_OFFSET_FROM_EXTRUDER 22      // X offset: -left  +right  [of the nozzle]
+#define Y_PROBE_OFFSET_FROM_EXTRUDER 4       // Y offset: -front +behind [the nozzle]
 #define Z_PROBE_OFFSET_FROM_EXTRUDER -1.02   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PBear_Clone
 Firmware and Stuff for Prusa Bear Clone
+
+# Download latest version of Marlin 2.0 and modify config files to match those in this repository
