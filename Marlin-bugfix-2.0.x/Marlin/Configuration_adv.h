0f0e6d02dbe67d5de43bd97a2a1ac1a188247435
sdp8483 <17242713+sdp8483@users.noreply.github.com>
2019-09-21 15:21:36 -0400

    update extruder current

--- Marlin-bugfix-2.0.x/Marlin/Configuration_adv.h
+++ Marlin-bugfix-2.0.x/Marlin/Configuration_adv.h
@@ -1778,7 +1778,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT    350
+    #define E0_CURRENT    400
     #define E0_MICROSTEPS  32
     #define E0_RSENSE    0.11
     #define E0_CHAIN_POS    0
