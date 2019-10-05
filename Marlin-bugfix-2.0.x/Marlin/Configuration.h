f6b3ccf2e763ca333234b43d62c4c571a0124ced
sdp8483 <17242713+sdp8483@users.noreply.github.com>
2019-10-05 16:41:28 -0400

    move a little faster when probing

--- Marlin-bugfix-2.0.x/Marlin/Configuration.h
+++ Marlin-bugfix-2.0.x/Marlin/Configuration.h
@@ -927,7 +927,7 @@
 #define MIN_PROBE_EDGE 5
 
 // X and Y axis travel speed (mm/m) between probes
-#define XY_PROBE_SPEED 30*60
+#define XY_PROBE_SPEED 35*60
 
 // Feedrate (mm/m) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
