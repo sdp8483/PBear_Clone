3029a9ac70341eb0874deca4ccbe2910236e7c0e
Sam <17242713+sdp8483@users.noreply.github.com>
2019-12-16 08:54:55 -0500

    increase Y current

--- Marlin-2.0.x/Marlin/Configuration_adv.h
+++ Marlin-2.0.x/Marlin/Configuration_adv.h
@@ -1896,7 +1896,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       450
+    #define Y_CURRENT       500
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
